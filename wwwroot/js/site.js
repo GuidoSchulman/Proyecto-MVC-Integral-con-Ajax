c0e9ec06466f20c0c8b242846e804da7b0b3aab0
GUIDO SCHULMAN <46961979@ort>
2022-10-19 12:04:45 -0300

    JavaScript

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,7 +11,6 @@ function BuscarIngredientes(){
     query1=$("#buscar").val();
     $.getJSON( 'https://api.spoonacular.com/food/ingredients/search?query='+query1+'&number=5&sort=calories&sortDirection=desc&apiKey=7f0f3940746741a4842ccd88ca650e43', function( data ) 
      {
-
         data.results.forEach(element => {
             console.log(element);
             $("#ingredientes").append("<p>"+element.name+"<p>")
