032b6dd94504996dca30544ef7c939e94a353ef9
GUIDO SCHULMAN <46961979@ort>
2022-10-19 12:04:06 -0300

    JavaScript

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
+/*
 fetch('https://api.spoonacular.com/food/ingredients/9266/information?apiKey=7f0f3940746741a4842ccd88ca650e43') //Pide a la API el ingrediente con el ID buscado
     .then(response => response.json())
     .then(data => console.log(data));
+*/
+function BuscarIngredientes(){
+    query1=$("#buscar").val();
+    $.getJSON( 'https://api.spoonacular.com/food/ingredients/search?query='+query1+'&number=5&sort=calories&sortDirection=desc&apiKey=7f0f3940746741a4842ccd88ca650e43', function( data ) 
+     {
 
-
-fetch('https://api.spoonacular.com/food/ingredients/search?query=banana&number=2&sort=calories&sortDirection=desc&apiKey=7f0f3940746741a4842ccd88ca650e43')
-.then(response => response.json())
-    .then(data => console.log(data));
+        data.results.forEach(element => {
+            console.log(element);
+            $("#ingredientes").append("<p>"+element.name+"<p>")
+        })
+     }
+    );
+    /*fetch('https://api.spoonacular.com/food/ingredients/search?query='+query1+'&number=5&sort=calories&sortDirection=desc&apiKey=7f0f3940746741a4842ccd88ca650e43')
+    .then(response => response.json())
+        .then(
+            data => console.log(data)
+            data.results.forEach(element => {
+                $("#ingredientes").html("<p>"+element+"<p>")
+            })
+        )
+*/
+}
