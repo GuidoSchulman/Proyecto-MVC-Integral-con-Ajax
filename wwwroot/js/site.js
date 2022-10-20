20b01546a4a301cb8cecb863c6ce335c40f45762
GUIDO SCHULMAN <46961979@ort>
2022-10-20 13:53:01 -0300

    Cartas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,24 +11,14 @@ function BuscarIngredientes(){
     query1=$("#buscar").val();
     $.getJSON( 'https://api.spoonacular.com/food/ingredients/search?query='+query1+'&number=5&sort=calories&sortDirection=desc&apiKey=7f0f3940746741a4842ccd88ca650e43', function( data ) 
      {
-        const container = document.getElementById('ingredientes')
         data.results.forEach(element => {
             console.log(element);
             $("#ingredientes").append(
-                container.innerHTML= `
-                ${container.innerHTML}
-                <div class='flip-card'>
-                    <div class='flip-card-inner'>
-                        <div class='flip-card-front'>
-                            <img src=${element.image} +  alt='Avatar' style='width:300px;height:300px;'>
-                        </div>
-                        <div class='flip-card-back'>
-                        </div>
-                    </div>
-                </div>
-                `
-            )
+                
 
+                "<div class='flip-card'><div class='flip-card-inner'><div class='flip-card-front'><img src=" + element.image + " alt='Avatar' style='width:300px;height:300px;'></div><div class='flip-card-back'></div></div></div>"
+
+                )
         })
      }
     );
